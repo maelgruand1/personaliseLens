2a943d416c6aa3c5fb88d8652a1b5ef19a33dd32
Maël Gruand <mrg0112@alu.medac.es>
2024-11-19 19:15:35 +0100

    push script

--- script.js
+++ script.js
@@ -15,18 +15,28 @@ function returnAccueil() {
   redirectTo = true;
   window.location.href = "index.html";
 }
-// Ici, tu peux ajouter des fonctionnalités JavaScript pour rendre ta galerie plus interactive,
-// comme un effet de zoom, un diaporama, ou une modal pour afficher les images en grand format.
+// Fonction pour ouvrir la modal
+function openModal(img) {
+  // Obtenez l'image et la légende
+  var modalImg = document.getElementById("img01");
+  var captionText = document.getElementById("caption");
+  modalImg.src = img.src;
+  captionText.innerHTML = img.alt;
+  // Affiche la modal
+  document.getElementById("myModal").style.display = "block";
+}
 
-// Par exemple, pour ajouter un effet de zoom basique :
+// Fonction pour fermer la modal
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+// Ajoute un événement click à chaque image pour ouvrir la modal
 const images = document.querySelectorAll('.gallery img');
 
 images.forEach(image => {
-    image.addEventListener('mouseover', () => {
-        image.style.transform = 'scale(1.2)';
-    });
-
-    image.addEventListener('mouseout', () => {
-        image.style.transform = 'scale(1)';
-    });
+  image.addEventListener('click', () => {
+    openModal(image);
+  });
 });
